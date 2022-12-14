c78dd227cc1e58b1a78cf5e521209a4c33d722e3
oksanaverkh <oksana-tyryatkina@yandex.ru>
2022-12-14 16:32:05 +0300

    Final commit

--- Task2.py
+++ Task2.py
@@ -1,13 +1,22 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = 0
-y = 0
-z = 0
+x = bool(input('Enter X (true or false): '))
+y = bool(input('Enter Y (true or false): '))
+z = bool(input('Enter Z (true or false): '))
 
-while x <= 1:
-    x += 1
-    while y <= 1:
-        y += 1
-        while z <= 1:
-            x or y or z == 
-            z += 1
+
+result = bool((not (x or y or z)) == (not x and not y and not z))
+print('')
+print(f'Result of the statement is {result}')
+
+
+# X	     Y	     Z	  ¬(X⋁Y⋁Z) ¬X⋀¬Y⋀¬Z result
+# true	true	true	false	false	true
+# true	true	false	false	false	true
+# true	false	true	false	false	true
+# true	false	false	false	false	true
+# false	true	true	false	false	true
+# false	true	false	false	false	true
+# false	false	true	false	false	true
+# false	false	false	true	true	true
+
